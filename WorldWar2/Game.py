fb0a17e728fda5ad7d9dc46eda96132f4687efc5
Artur Babkiewicz <arturbabkiewicz@gmail.com>
2020-01-13 20:52:39 +0100

    Created bullet class

--- WorldWar2/Game.py
+++ WorldWar2/Game.py
@@ -1,4 +1,5 @@
 import pygame
+
 pygame.init()
 
 windowWidth = 1280
@@ -7,86 +8,102 @@ windowHeight = 720
 win = pygame.display.set_mode((windowWidth, windowHeight))
 pygame.display.set_caption("World War 2")
 
-walkRight = [pygame.image.load('images/AgoingR1.png'),pygame.image.load('images/AgoingR2.png'),pygame.image.load('images/AgoingR3.png')]
-walkLeft = [pygame.image.load('images/AgoingL1.png'),pygame.image.load('images/AgoingL2.png'),pygame.image.load('images/AgoingL3.png')]
+walkRight = [pygame.image.load('images/AgoingR1.png'), pygame.image.load('images/AgoingR2.png'),
+             pygame.image.load('images/AgoingR3.png')]
+walkLeft = [pygame.image.load('images/AgoingL1.png'), pygame.image.load('images/AgoingL2.png'),
+            pygame.image.load('images/AgoingL3.png')]
 charL = pygame.image.load('images/AstandingL.png')
 charR = pygame.image.load('images/AstandingR.png')
-crouchRight = [pygame.image.load('images/AcrouchR1.png'),pygame.image.load('images/AcrouchR2.png')]
-crouchLeft = [pygame.image.load('images/AcrouchL1.png'),pygame.image.load('images/AcrouchL2.png')]
 bg = pygame.image.load('images/omaha.jpg')
 
 clock = pygame.time.Clock()
 
-x = 50
-y = 50
-width = 40
-height = 40
-vel = 2
-left = False
-right = False
-movingVer = False
-sLeft = False
-sRight = True
-walkCount = 0
+
+class player(object):
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 2
+        self.left = False
+        self.right = False
+        self.movingVer = False
+        self.sLeft = False
+        self.sRight = True
+        self.walkCount = 0
+
+    def draw(self, win):
+        if self.walkCount + 0 >= 9:
+            self.walkCount = 0
+        if self.left:
+            win.blit(walkLeft[self.walkCount // 3], (self.x, self.y))
+            self.walkCount += 1
+        elif self.right:
+            win.blit(walkRight[self.walkCount // 3], (self.x, self.y))
+            self.walkCount += 1
+        elif self.movingVer and self.sRight:
+            win.blit(walkRight[self.walkCount // 3], (self.x, self.y))
+            self.walkCount += 1
+        elif self.movingVer and self.sLeft:
+            win.blit(walkLeft[self.walkCount // 3], (self.x, self.y))
+            self.walkCount += 1
+        else:
+            if self.sRight:
+                win.blit(charR, (self.x, self.y))
+            else:
+                win.blit(charL, (self.x, self.y))
+
+class projectile(object):
+    def __init__(self,x,y,radius,color,facing):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.facing = facing
+        self.vel = 3 * facing
+
+    def draw (self, win):
+        pygame.draw.circle(win, self.color, (self.x,self.y), self.radius)
 
 def redrawGameWindow():
-    global walkCount
-    win.blit(bg, (0,0))
-
-    if walkCount + 1 >= 9:
-        walkCount = 0
-    if left:
-        win.blit(walkLeft[walkCount//3], (x,y))
-        walkCount += 1
-    elif right:
-        win.blit(walkRight[walkCount//3], (x,y))
-        walkCount += 1
-    elif movingVer and sRight:
-        win.blit(walkRight[walkCount//3], (x,y))
-        walkCount += 1
-    elif movingVer and sLeft:
-        win.blit(walkLeft[walkCount//3], (x,y))
-        walkCount += 1
-    else:
-        if sRight:
-            win.blit(charR, (x,y))
-        else:
-            win.blit(charL, (x,y))
-
+    win.blit(bg, (0, 0))
+    player.draw(win)
     pygame.display.update()
 
-#mainloop
+
+# mainloop
+player = player(10, 10, 40, 40)
 run = True
 while run:
-        clock.tick(30)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and x > 0:
-            x -= vel
-            left = True
-            right = False
-            sRight = False
-            sLeft = True
-        elif keys[pygame.K_RIGHT] and x < windowWidth - width:
-            x += vel
-            left = False
-            right = True
-            sRight = True
-            sLeft = False
-        else :
-            right = False
-            left = False
-        if keys[pygame.K_UP] and y > 0:
-            y -= vel
-            movingVer = True
-        elif keys[pygame.K_DOWN] and y < windowHeight - height:
-            y += vel
-            movingVer = True
-        else:
-            movingVer = False
-        redrawGameWindow()
+    clock.tick(30)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and player.x:
+        player.x -= player.vel
+        player.left = True
+        player.right = False
+        player.sRight = False
+        player.sLeft = True
+    elif keys[pygame.K_RIGHT] and player.x < windowWidth - player.width:
+        player.x += player.vel
+        player.left = False
+        player.right = True
+        player.sRight = True
+        player.sLeft = False
+    else:
+        player.right = False
+        player.left = False
+    if keys[pygame.K_UP] and player.y > 0:
+        player.y -= player.vel
+        player.movingVer = True
+    elif keys[pygame.K_DOWN] and player.y < windowHeight - player.height:
+        player.y += player.vel
+        player.movingVer = True
+    else:
+        player.movingVer = False
+    redrawGameWindow()
 
 pygame.quit()
